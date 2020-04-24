5bf048a9f986fd89b9b9ddfc350662fefddaf289
staris <sungdoolim@naver.com>
2020-04-24 12:18:12 +0900

    m

---
+++ javapractice/src/week6/bonus_node.java
@@ -0,0 +1,14 @@
+package week6;
+
+public class bonus_node {
+
+	bonus_node right;
+	bonus_node left;
+	bonus_node m1;
+	bonus_node m2;
+	
+	int r;
+	int l;
+	int m;
+	
+}
--- javapractice/src/week6/prac.java
+++ javapractice/src/week6/prac.java
@@ -1,19 +1,20 @@
 package week6;
 
+import java.security.Provider;
+import java.security.Security;
+
 public class prac {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		node a=new node();
-		node t=a;
-		System.out.println(a.value);
-		System.out.println(t.value);
-		t.value=10;
-		t.left=new node();
-		System.out.println(a.value);
-		System.out.println(t.value);
-		System.out.println(a.left.value);
+		 Provider provider = Security.getProvider("BC");
+		  if (provider != null){
+		   System.out.println("Bouncy Castle provider is available");
+		   System.out.println(provider.getInfo());
+		  } else {
+		   System.out.println("Bouncy Castle provider is NOT available");
+		  }
 	}
 
 }
