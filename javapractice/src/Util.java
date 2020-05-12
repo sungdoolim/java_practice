fd742b2905cf77bd67de079616323a49ca03ddb7
staris <sungdoolim@naver.com>
2020-05-12 18:52:14 +0900

    block with java

---
+++ javapractice/src/Util.java
@@ -0,0 +1,25 @@
+import java.security.MessageDigest;
+
+public class Util {
+
+	public static String getHash(String input) {
+		StringBuffer result=new StringBuffer();
+		try {
+			MessageDigest md=MessageDigest.getInstance("SHA-256");
+			md.update(input.getBytes());
+			byte bytes[]=md.digest();
+			for(int i=0;i<bytes.length;i++) {
+				result.append(Integer.toString((bytes[i]&0xff)+0x100,16).substring(1));
+				
+			}
+			
+			
+	} catch (Exception e) {
+			// TODO: handle exception
+		e.printStackTrace();
+		}
+		return result.toString();
+	}
+	
+	
+}
